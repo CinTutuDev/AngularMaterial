2eeb6f3e536f9ab1efa530e9398f2e1f13795218
cinta <cintatafur@gmail.com>
2023-03-30 18:58:47 +0200

    ✅ Add(<router-outlet):cards en el html compnentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanelComponent } from './panel/panel.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: PanelComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: '404',
+        component: ErrorComponent
+      },
+      {
+        path: '**',
+        redirectTo: '404',
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
